bf5c58000a2a169bdfa35257673df2dae6bca860
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2025-02-23 21:57:21 -0400

    feat(api): add `OpenAPI` dependency and configuration class

---
+++ src/main/java/flux/logistic/infrastructure/configuration/swagger/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package flux.logistic.infrastructure.configuration.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Logistics API")
+                        .version("1.0")
+                        .description("An application for any drugstore-related operational approval process."));
+    }
+}
