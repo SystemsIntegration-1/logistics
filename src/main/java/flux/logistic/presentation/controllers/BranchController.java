efce67ba99ead5dc9eb60d4e03eb9231bdcf598c
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2025-02-23 23:24:55 -0400

    feat: update branch endpoints

---
+++ src/main/java/flux/logistic/presentation/controllers/BranchController.java
@@ -0,0 +1,47 @@
+package flux.logistic.presentation.controllers;
+
+import flux.logistic.application.requests.BranchCreateRequest;
+import flux.logistic.application.responses.BranchResponse;
+import flux.logistic.application.services.contracts.IBranchService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/branches")
+public class BranchController {
+  private final IBranchService branchService;
+
+  public BranchController(IBranchService branchService) {
+    this.branchService = branchService;
+  }
+
+  @GetMapping("/{branchId}")
+  public ResponseEntity<?> getBranchById(@PathVariable("branchId") UUID branchId) {
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(branchService.getBranchById(branchId));
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getAllBranches() {
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(branchService.findAllBranches());
+  }
+
+  @PostMapping
+  public ResponseEntity<BranchResponse> createBranch(@RequestBody BranchCreateRequest branchCreateRequest) {
+    BranchResponse createdBranch = branchService.createBranch(branchCreateRequest);
+    return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(createdBranch);
+  }
+
+  @PutMapping
+  public ResponseEntity<BranchResponse> updateBranch(@RequestBody BranchCreateRequest branchCreateRequest) {
+    return null;
+  }
+}
